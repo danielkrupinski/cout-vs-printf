8240205348312fda2f6843f9034a540b0f7a4bb7
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-08 13:24:59 +0200

    Update main() function

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,11 @@ using namespace std;
 
 int main()
 {
+    cout << "Enter amount of test: ";
+    int tests {0};
+    cin >> tests;
+    Integer test {tests};
+    cin.ignore();
+    getchar();
     return 0;
 }
