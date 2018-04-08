fa73eadbbb14cb213af67a4484b38088faa35b8c
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-08 13:04:27 +0200

    Add time measurement

--- src/integer.cpp
+++ src/integer.cpp
@@ -1,4 +1,6 @@
 #include "../include/integer.h"
+#include <iostream>
+#include <chrono>
 
 Integer::Integer(int a)
     : amount{a}
@@ -9,7 +11,11 @@ Integer::Integer(int a)
 
 Integer::Test()
 {
-    for (int i=0; i!=amount; ++i)
-        for (int i=0; i!=1000000; ++i);
-
+    for (int i=0; i!=amount; ++i) {
+        auto start = std::chrono::high_resolution_clock::now();
+        for (int j=0; j!=1000000; ++j)
+            std::cout << j;
+        auto end = std::chrono::high_resolution_clock::now();
+        results.push_back(std::chrono::duration_cast<milliseconds>(end-start).count());
+    }
 }
