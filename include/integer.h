7a1ae79745ce036b37966b18e295a9aa223a32e6
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-08 12:37:01 +0200

    Add Integer class declaration

--- include/integer.h
+++ include/integer.h
@@ -1,4 +1,12 @@
 #ifndef INTEGER_H
 #define INTEGER_H
 
+class Integer {
+public:
+    Integer(int a) : amount{a} {}
+private:
+    int amount {0};
+    std::vector<int> results;
+};
+
 #endif // INTEGER_H
