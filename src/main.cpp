9696b4e4ed91caaf06a6c2e6cec43d6e237a5265
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-09 20:21:28 +0200

    Remove using namespace std directive

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include "../include/integer.h"
 
-using namespace std;
-
 int main()
 {
-    cout << "Enter amount of test: ";
+    std::cout << "Enter amount of test: ";
     int tests {0};
-    cin >> tests;
+    std::cin >> tests;
     Integer test {tests};
     cin.ignore();
     getchar();
