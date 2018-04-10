83ebb7a8f571f64a5ef0951fbd27316901512de6
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-10 16:08:10 +0200

    Add string class

--- include/string.h
+++ include/string.h
@@ -1,4 +1,19 @@
 #ifndef STRING_H
 #define STRING_H
 
+#include <vector>
+
+class String {
+public:
+    explicit String(int a);
+    void testCout();
+    void testPrintf();
+    void print();
+    void clearScreen();
+private:
+    int amount {0};
+    std::vector<int> resultsCout;
+    std::vector<int> resultsPrintf;
+};
+
 #endif // STRING_H
