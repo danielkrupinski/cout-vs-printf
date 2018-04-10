d5c000da77d94b27098db00fe2cf67f42ed93edf
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-10 17:08:28 +0200

    Update testPrintf() and testCout() methods

--- src/string.cpp
+++ src/string.cpp
@@ -16,7 +16,7 @@ void Integer::testCout()
     for (int i=0; i!=amount; ++i) {
         auto start = std::chrono::high_resolution_clock::now();
         for (int j=0; j!=1000000; ++j)
-            std::cout << j;
+            std::cout << "abcdef";
         auto end = std::chrono::high_resolution_clock::now();
         resultsCout.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count());
     }
@@ -28,7 +28,7 @@ void Integer::testPrintf()
     for (int i=0; i!=amount; ++i) {
         auto start = std::chrono::high_resolution_clock::now();
         for (int j=0; j!=1000000; ++j)
-            printf("%d",j);
+            printf("%s","abcdef");
         auto end = std::chrono::high_resolution_clock::now();
         resultsPrintf.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count());
     }
