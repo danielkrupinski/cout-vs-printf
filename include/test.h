3fe7bafbeeeb38b87f20cc20bbecea449f3844e7
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-10 16:13:26 +0200

    Update virtual class Test

--- include/test.h
+++ include/test.h
@@ -2,7 +2,16 @@
 #define TEST_H
 
 class Test {
-
+public:
+    virtual Test();
+    virtual void testCout();
+    virtual void testPrintf();
+    virtual void print();
+    virtual void clearScreen();
+private:
+    int amount {0};
+    std::vector<int> resultsCout;
+    std::vector<int> resultsPrintf;
 };
 
 #endif // TEST_H
