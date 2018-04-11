905f04af58db84c642d38065622263308766471b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-11 18:21:50 +0200

    Add void Test::clearScreen() definition

--- src/test.cpp
+++ src/test.cpp
@@ -1,2 +1,12 @@
 #include "../include/test.h"
 #include <iostream>
+
+void Test::clearScreen()
+{
+    std::cout << std::endl;
+    #ifdef _WIN32
+        system("cls");
+    #elif defined __linux__
+        system("clear");
+    #endif
+}
