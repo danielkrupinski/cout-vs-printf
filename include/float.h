15ac201b48897207833f5a92f3c9d7f6c1daea5b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-12 15:42:10 +0200

    Add Float class

--- include/float.h
+++ include/float.h
@@ -1,4 +1,20 @@
 #ifndef FLOAT_H
 #define FLOAT_H
 
+#include <vector>
+#include "test.h"
+
+class Float : public Test {
+public:
+    explicit Integer(int a);
+    void testCout();
+    void testPrintf();
+    void print();
+    //void clearScreen();
+private:
+    int amount {0};
+    std::vector<int> resultsCout;
+    std::vector<int> resultsPrintf;
+};
+
 #endif // FLOAT_H
