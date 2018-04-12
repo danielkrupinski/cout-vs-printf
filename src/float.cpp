77d0a9dc3d31ed652a3cca6c0069a4b471344d49
Daniel Krupiński <danielkrupinski@outlook.com>
2018-04-12 15:45:46 +0200

    Update tests

--- src/float.cpp
+++ src/float.cpp
@@ -16,7 +16,7 @@ void Float::testCout()
     for (int i=0; i!=amount; ++i) {
         auto start = std::chrono::high_resolution_clock::now();
         for (int j=0; j!=1000000; ++j)
-            std::cout << j;
+            std::cout << j+0.12345;
         auto end = std::chrono::high_resolution_clock::now();
         resultsCout.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count());
     }
@@ -28,7 +28,7 @@ void Float::testPrintf()
     for (int i=0; i!=amount; ++i) {
         auto start = std::chrono::high_resolution_clock::now();
         for (int j=0; j!=1000000; ++j)
-            printf("%d",j);
+            printf("%f",j+0.12345);
         auto end = std::chrono::high_resolution_clock::now();
         resultsPrintf.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count());
     }
